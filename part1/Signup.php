4e9ce330a6a31c629fad806f446ad76f3299b00a
DanielTran3 <dtran3@ualberta.ca>
2017-10-13 22:19:09 -0600

    working on sql

---
+++ part1/Signup.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset = "utf-8">
+      <body>
+            <?php
+                  $username = $_POST["signupUsername"];
+                  $password = $_POST["signupPassword"];
+
+                  $query = "INSERT INTO credentials (username, password) VALUES ("  . $username . ", " . $password . ")";
+
+                  // Connect to MySQL
+                  if (!($database = mysql_connect("localhost", "iw3htp", "password"))) {
+                        die("Could not connect to database </body></html>");
+                  }
+
+                  // open Products database
+                  if ( !mysql_select_db( "products", $database ) )
+                  die( "Could not open products database </body></html>" );
+
+                  // query Products database
+                  if ( !( $result = mysql_query( $query, $database ) ) ) 
+                  {
+                  print( "<p>Could not execute query!</p>" );
+                  die( mysql_error() . "</body></html>" );
+                  } // end if
+
+                  mysql_close( $database );
+            ?>      
+      </body>
+</head>
---
+++ part1/create_tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE credentials (
+	username VARCHAR(30) NOT NULL , 
+	password VARCHAR(30) NOT NULL,
+	PRIMARY KEY(username));
+CREATE TABLE bookmarks (
+	username VARCHAR(30) NOT NULL , 
+	url VARCHAR(30) NOT NULL,
+	FOREIGN KEY (username) REFERENCES credentials(username),
+	UNIQUE (username, url));
